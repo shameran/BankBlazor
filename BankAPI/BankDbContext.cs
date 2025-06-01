0e41ac8479e5ec1980f390f5f29b6d351241ec78
shameran karim <shameran.karim@hotmail.com>
2025-06-01 12:50:08 +0200

    test

--- BankAPI/BankDbContext.cs
+++ BankAPI/BankDbContext.cs
@@ -5,13 +5,19 @@ namespace BankAPI
 {
     public class BankDbContext : DbContext
     {
-        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
-        {
-        }
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options) { }
 
-      
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Accounts)
+                .WithOne(a => a.Customer)
+                .HasForeignKey(a => a.CustomerId);
+        }
     }
 }
--- BankBlazor.Shared/Models/Account.cs
+++ BankBlazor.Shared/Models/Account.cs
@@ -5,8 +5,7 @@
         public int AccountId { get; set; }
         public int CustomerId { get; set; }
         public decimal Balance { get; set; }
-
-        public string AccountType { get; set; }
+        public string AccountType { get; set; } = string.Empty;
 
         public Customer? Customer { get; set; }
         public ICollection<Transaction>? Transactions { get; set; }
